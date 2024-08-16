03ba0eba83527e6d76114f314d4bad30b733e57c
Tiago França <t.iago_196@hotmail.com>
2024-08-16 17:11:33 -0300

    Add project files.

---
+++ Asynchronous_Factory_Method_Fluent/Foo.cs
@@ -0,0 +1,31 @@
+﻿public class Foo
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+
+    protected Foo()
+    {
+        Id = 0; 
+        Name = "Default"; 
+    }
+
+    private async Task<Foo> InitAsync()
+    {
+        await Task.Delay(100);
+        Id = await GetIdFromServiceAsync();
+        Name = "Initialized";
+        return this;
+    }
+
+    public static Task<Foo> CreateAsync()
+    {
+        var result = new Foo();
+        return result.InitAsync();
+    }
+
+    private async Task<int> GetIdFromServiceAsync()
+    {
+        await Task.Delay(50);
+        return 42;
+    }
+}
---
+++ Asynchronous_Factory_Method_Fluent/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        var foo = await Foo.CreateAsync();
+        Console.WriteLine($"Foo initialized with Id = {foo.Id}, Name = {foo.Name}");
+    }
+}
